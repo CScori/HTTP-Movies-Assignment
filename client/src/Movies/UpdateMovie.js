f1ea3af177b58de3c39c63ea33b495e2922a3f3c
CScori <amilanotte1366@gmail.com>
2019-10-17 14:26:52 -0500

    wrote form

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,63 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
-const UpdateMovie = () => {
+const UpdateMovie = (props) => {
+    const intake = {
+        title: "", 
+        director: "", 
+        metascore: "", 
+        stars: ""
+    }
+    const [form, setForm] = useState(intake)
     const handleSubmit = e => {
         e.preventDefault()
     }
-    // const changeHandler = e => {
-    //     [e.target.name]; e.target.value
-    // }
+    const changeHandler = e => {
+        e.persist();
+        let value = e.target.value;
+        if (e.target.name === 'title') {
+          value = parseInt(value, 10);
+        }
     return (
         <div>
                 <h2>Update Item</h2>
-                {/* <form onSubmit={handleSubmit}>
+               <form onSubmit={handleSubmit}>
                     <input
                         type="text"
-                        name="name"
+                        name="title"
                         onChange={changeHandler}
-                        placeholder="name"
-                        value={}
+                        placeholder="title"
+                        value={props.title}
                     />
-                    <div className="baseline" />
-
+                    <input
+                        type="text"
+                        name="director"
+                        onChange={changeHandler}
+                        placeholder="Director"
+                        value={props.director}
+                    />
                     <input
                         type="number"
-                        name="price"
+                        name="metascore"
                         onChange={changeHandler}
-                        placeholder="Price"
-                        value={}
+                        placeholder="metascore"
+                        value={props.metascore}
                     />
-                    <div className="baseline" />
-
                     <input
                         type="string"
-                        name="imageUrl"
+                        name="star"
                         onChange={changeHandler}
-                        placeholder="Image"
-                        value={}
+                        placeholder="Star"
+                        value={props.star}
                     />
-                    <div className="baseline" />
-
-                    <input
-                        type="string"
-                        name="description"
-                        onChange={changeHandler}
-                        placeholder="Description"
-                        value={}
-                    />
-                    <div className="baseline" />
-
-                    <input
-                        type="string"
-                        name="shipping"
-                        onChange={changeHandler}
-                        placeholder="Shipping"
-                        value={}
-                    /> */}
-
+                
                     <button>Update</button>
-                {/* </form> */}
+                </form>
             </div>
             )
         }
